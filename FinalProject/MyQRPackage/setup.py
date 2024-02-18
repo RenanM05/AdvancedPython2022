4b9fa95fd640869137e96e4d26d2e73b65e5934d
Renan Maciel <r.maciel.000@gmail.com>
2024-02-18 01:29:47 +0100

    Add files via upload

--- FinalProject/MyQRPackage/setup.py
+++ FinalProject/MyQRPackage/setup.py
@@ -1,24 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MyQRPackage', 
-    version='0.1.0',  
+    name='MyQRPackage',
+    version='0.1',
     author='Renan Maciel',  
     author_email='renan.maciel@physics.uu.se',  
-    description='A package for QR decomposition using Householder reflections', 
-    long_description=open('README.md').read(),  
-    long_description_content_type='text/markdown', 
-    url='https://github.com/RenanM05/AdvancedPython2022/tree/main/FinalProject/MyQRPackage',  
-    packages=find_packages(),  
+    url='https://github.com/RenanM05/AdvancedPython2022/tree/main/FinalProject/MyQRPackage',      
+    description='A package for QR decomposition using Householder reflections.',
     classifiers=[
+        'License :: OSI Approved :: MIT License',        
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.11',
-        'Operating System :: OS Independent',
+        'Operating System :: OS Independent',        
     ],
-    # Minimum version requirement of the package
-    python_requires='>=3.6', 
+    packages=find_packages(),
+    python_requires='>=3.6',
     install_requires=[
         'numpy',  
     ],
@@ -27,6 +25,4 @@ setup(
             'pytest',
         ],
     },
-    include_package_data=True, 
-    zip_safe=False, 
 )
